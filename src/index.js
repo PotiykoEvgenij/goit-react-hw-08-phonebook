8ba250d692d2e4f915c6beefb732034d5e1da653
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-07-23 23:37:54 +0300

    hw8

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='goit-react-hw-08-phonebook'>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename='goit-react-hw-08-phonebook'>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
